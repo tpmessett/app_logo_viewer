a4b9e717695ebfeaa6ae021dc67c9be7d4f34be9
thomas messett <thomas.messett@stampapp.me>
2022-07-02 18:51:39 +0100

    got icons working

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,31 @@
 <template>
   <h1>App Icon Preview:</h1>
-  <img v-bind:src="image" />
+  <img class="main-icon" v-if="image" v-bind:src="image" />
+  {{text}}
   <form>
-  <input ref="file" @change="handleFileUpload()"  type="file">
-</form>
+    <input ref="file" @change="handleFileUpload()"  type="file">
+    <input v-model="text" placeholder="Enter App Name">
+  </form>
   <div class="devices">
     <div class="iphone phone">
+      <div class="icon-iphone">
+        <img class="iconi" v-if="image" v-bind:src="image"/>
+      </div>
     </div>
-    <div class="samsung phone">
+    <div class="android phone">
+      <div class="icon-android">
+          <img class="icona" v-if="image" v-bind:src="image"/>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-
+import { ref } from 'vue'
 export default ({
-
   setup() {
+    const text = ref('')
+    const image = localStorage.getItem('imageKey')
     const handleFileUpload = async() => {
       const file = document.querySelector('input[type=file]').files[0]
       const reader = new FileReader()
@@ -28,13 +37,14 @@ export default ({
       }
       reader.readAsDataURL(file);
       console.log(file)
-
+      console.log(image)
+      console.log(image.value)
+      location.reload();
     }
-
-    const image = localStorage.getItem('imageKey')
     return {
       handleFileUpload,
-      image
+      image,
+      text
     };
   }
 });
@@ -49,6 +59,34 @@ export default ({
   text-align: center;
   color: #2c3e50;
 }
+.main-icon {
+  max-height: 50%;
+  max-width:  50%;
+}
+.icon-iphone {
+  background-color: white;
+  width: 50px;
+  height: 50px;
+  position: relative;
+  bottom:  -490px;
+  left: 22px;
+  border-radius: 20%;
+  text-align:  center;
+  overflow: hidden;
+}
+
+.icon-android {
+  background-color: white;
+  width: 42px;
+  height: 42px;
+  position: relative;
+  bottom:  -580px;
+  right: -263px;
+  border-radius: 50%;
+  text-align:  center;
+  overflow: hidden;
+  box-shadow: 0 0 3px #ccc;
+}
 
 .devices {
   display: flex;
@@ -59,20 +97,49 @@ export default ({
 .iphone {
   background-image: url(./assets/iphone.png);
   margin: 16px;
-}
-
-.samsung {
-  background-image: url(./assets/samsung.png);
-  margin: 16px;
-
-}
-
-.phone {
   display: block;
   width: 326px;
   height: 723px;
   background-repeat: no-repeat;
   background-position: center;
+}
+
+.android {
+  background-image: url(./assets/pixel.png);
+  margin: 16px;
+  display: block;
+  width: 326px;
+  height: 723px;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: contain;
+}
+
+.iconi{
+    max-height:50px;
+    max-width:50px;
+    height:auto;
+    width:auto;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    margin: auto;
+}
+
+.icona{
+    max-height:38px;
+    max-width:38px;
+
+    height:auto;
+    width:auto;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    margin: auto;
 }
 
 @media only screen and (max-width: 992px) {
